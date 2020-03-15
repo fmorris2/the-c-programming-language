4ff7ef1904a2cc2fdefda1a7ce472bd67355ee84
fmorris2 <fmorris2@oswego.edu>
2020-03-15 14:39:09 +0000

    Fahrenheit --> Celsius conversion table is complete.

--- chapter-one/ex1-3.c
+++ chapter-one/ex1-3.c
@@ -1,5 +1,28 @@
 #include <stdio.h>
 
-main() {
+float convertFahrenheitToCelsius(float fahr) {
+	return (5.0 / 9.0) * (fahr - 32.0);
+}
 
+void printFahrToCelsConversionTable(float minTemp, float maxTemp,
+	float stepSize) {
+	
+	//header
+	printf("%50s\n", "Fahrenheit --> Celsius Conversion Table");
+	printf("%50s\n\n", "---------------------------------------");
+	printf("%18s\t%18s\n", "F", "C");
+
+	//table contents
+	float currentTemp = minTemp;
+	for(; currentTemp <= maxTemp; currentTemp += stepSize) {
+		float celsius = convertFahrenheitToCelsius(currentTemp);
+		printf("%20.1f\t%20.1f\n", currentTemp, celsius);
+	}	
+}
+
+/*
+* Modify the temperature conversion program to print a heading above the table.
+*/
+main() {
+	printFahrToCelsConversionTable(0,200,20);
 }
