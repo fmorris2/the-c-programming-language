326cdcdac97c3ce0e6051664b416a22bb41cebd1
fmorris2 <fmorris2@oswego.edu>
2020-03-17 01:40:10 +0000

    Chapter one exercises are complete.

--- chapter-one/temp-conversion.c
+++ chapter-one/temp-conversion.c
@@ -18,20 +18,42 @@ float convertCelsiusToFahrenheit(float cels) {
 	return ((9.0 / 5.0) * cels) + 32.0;
 }
 
-void printFahrToCelsConversionTable(float minTemp, float maxTemp,
-	float stepSize) {
-	
+void printConversionTable(char* title, char fromUnit, char toUnit,
+float (*conversionFunc)(float),  float minTemp, float maxTemp,
+float stepSize) {
 	//header
-	printf("%50s\n", "Fahrenheit --> Celsius Conversion Table");
+	printf("%50s\n", title);
 	printf("%50s\n\n", "---------------------------------------");
-	printf("%18s\t%18s\n", "F", "C");
+	printf("%18c\t%18c\n", fromUnit, toUnit);
 
 	//table contents
 	float currentTemp = minTemp;
 	for(; currentTemp <= maxTemp; currentTemp += stepSize) {
-		float celsius = convertFahrenheitToCelsius(currentTemp);
-		printf("%20.1f\t%20.1f\n", currentTemp, celsius);
-	}	
+		float convertedTemp = (*conversionFunc)(currentTemp);
+		printf("%20.1f\t%20.1f\n", currentTemp, convertedTemp);
+	}
+}
+
+void printFahrToCelsConversionTable(float minTemp, float maxTemp,
+	float stepSize) {
+	
+	char* title = "Fahrenheit to Celsius Conversion Table";
+	char fromUnit = 'F';
+	char toUnit = 'C';
+
+	printConversionTable(title, fromUnit, toUnit, &convertFahrenheitToCelsius,
+		minTemp, maxTemp, stepSize); 		
+}
+
+void printCelsToFahrConversionTable(float minTemp, float maxTemp,
+	float stepSize) {
+	
+	char* title = "Celsius to Fahrenheit Conversion Table";
+	char fromUnit = 'C';
+	char toUnit = 'F';
+
+	printConversionTable(title, fromUnit, toUnit, &convertCelsiusToFahrenheit,
+		minTemp, maxTemp, stepSize); 		
 }
 
 /*
@@ -39,4 +61,5 @@ void printFahrToCelsConversionTable(float minTemp, float maxTemp,
 */
 main() {
 	printFahrToCelsConversionTable(0,200,20);
+	printCelsToFahrConversionTable(0,200,20);
 }
