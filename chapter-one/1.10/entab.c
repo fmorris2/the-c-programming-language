b5628894e0ec6617c164e0fbc99191e3aa96f50d
fmorris2 <fmorris2@oswego.edu>
2020-04-05 21:32:40 +0000

    Skelston for Section 1.10 Exercise 1-21.

---
+++ chapter-one/1.10/entab.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*
+*	Exercise 1-21
+*		Write a program "entab" that replaces strings of blanks by
+*		the minimum number of tabs and blanks to achieve the same
+*		spacing. Use the same tab stops as for "detab". When either
+*		a tab or a single blank would suffice to reach a tab stop,
+*		which should be given preference?
+*
+*	Personal Note
+*		When either a tab or a single blank would suffice to reach
+*		a tab stop, a tab should be given preference. The reason for
+*		this is so that the code remains consistent in intent. We're
+*		"encoding" the input with tabs, so we should remain consistent
+*		in that mission.
+*/
+
+#define DEFAULT_TAB_STOP_LENGTH 8
+
+int main() {
+	
+	
+	return 0;
+}
